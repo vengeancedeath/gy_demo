1c076801f6de0e3e2596315e47c9a68489d049b0
IMKTNT\weiping.sheng <swp18258685707@outlook.com>
2026-07-09 17:19:22 +0800

    feat:新增FreeRTOS/事件标志组

---
+++ FreeRTOS/事件标志组/freertos_task_event_groups.h
@@ -0,0 +1,22 @@
+#ifndef __FREERTOS_TASK_EVENT_GROUPS_H__
+#define __FREERTOS_TASK_EVENT_GROUPS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "logger.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "event_groups.h"
+
+
+void freertos_start_event_groups(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __EEFLASH_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
