41ef96b4e91115c7fcb8f82c280751e1ff252da3
IMKTNT\weiping.sheng <swp18258685707@outlook.com>
2026-07-17 10:14:52 +0800

    feat:新增FreeRTOS/任务通知/模拟事件标志组 新增FreeRTOS/任务通知/模拟消息队列 新增FreeRTOS/任务通知/模拟信号量

---
+++ FreeRTOS/任务通知/模拟消息队列/freertos_task_noti_queue.h
@@ -0,0 +1,22 @@
+#ifndef __FREERTOS_TASK_NOTI_QUEUE_H__
+#define __FREERTOS_TASK_NOTI_QUEUE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "logger.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+
+void freertos_start_task_noti_queue(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __EEFLASH_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
